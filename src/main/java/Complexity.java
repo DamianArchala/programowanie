ff3641264618566fb376de5129e2870ce1e13cd9
Damian Archala <jumper5356@gmail.com>
2020-02-15 10:15:38 +0100

    Added Table, PrintTable

--- src/main/java/Complexity.java
+++ src/main/java/Complexity.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
-
 public class Complexity {
+        public static void hello() {
+            System.out.println("Hello world!");
+        }
+
+    public static void main(String[] args) {
+    }
 }
--- src/main/java/tablice/Table.java
+++ src/main/java/tablice/Table.java
@@ -2,12 +2,45 @@ package tablice;
 
 public class Table {
     public static void main(String[] args) {
-        PrintTable[] table = new PrintTable[10];    //deklaracja tablicy jako obiekt
-        PrintTable[] table2 = new PrintTable[10];    //deklaracja tablicy2 jako obiekt
-        PrintTable[] table3 = new PrintTable[10];    //deklaracja tablicy3 jako obiekt
-        for (int i = 0; i < 10; i++) {
-            table[i] = new PrintTable();
-            table[i].value = i;
+//        PrintTable[] table = new PrintTable[10];
+        int[] table = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int[] table2 = {5, 10, 15, 20, 25, 30, 35, 40, 45, 50};
+        odKonca(table);
+        parzyste(table2);
+        sumaPoprzednich(table);
+        sumaWiekszychOd5(table2);
+    }
+
+    public static void odKonca(int[] table) {
+        for (int i = 9; i > 0; i--) {
+            System.out.print(table[i] + ", ");
+        }
+        System.out.println();
+    }
+
+    public static void parzyste(int[] table) {
+        for (int i = 0; i < table.length; i++) {
+            if (i % 2 != 0) System.out.print(table[i] + ", ");
+        }
+        System.out.println();
+    }
+
+    public static void sumaPoprzednich(int[] table) {
+        for (int i = 0; i < table.length; i++) {
+            int a = 0;
+            for (int j = 0; j <= i; j++) {
+                a += table[j];
+            }
+            System.out.print(a + ", ");
+        }
+        System.out.println();
+    }
+
+    public static void sumaWiekszychOd5(int[] table) {
+        for (int i = 0, a = 0; i < table.length; i++) {
+            if (table[i] > 5) a += table[i];
+            if (i == table.length - 1) System.out.println(a);
         }
     }
 }
+
