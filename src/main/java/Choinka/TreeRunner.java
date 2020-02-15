0f18a10dd45b90518be862e51adb4f4289fb94cf
Damian Archala <jumper5356@gmail.com>
2020-02-15 12:18:03 +0100

    choinka dziala

--- src/main/java/Choinka/TreeRunner.java
+++ src/main/java/Choinka/TreeRunner.java
@@ -10,9 +10,11 @@ public class TreeRunner {
         tree.size = input.nextInt();
         System.out.println("Input tree type:");
         tree.type = input.next();
-        for (int i = 0; i < tree.size; i++) {
-            for (int k = 0; k < ((tree.size) - 1 - (2 * i)); k++) {
-                System.out.print(" ");
+        for (int i = 0; i <= tree.size; i++) {
+            for (int k = 0; k < ((tree.size*2)-((i * 2) - 1))/2; k++) {
+                for (int j = 0; j <tree.type.length(); j++) {
+                    System.out.print(" ");
+                }
             }
             for (int k = 0; k < (i * 2) - 1; k++) {
                 System.out.print(tree.type);
